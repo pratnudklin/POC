3e85f5fd0b8798d6ab5815766d8af21ea5b5a616
Prat Nudklin <prat.nudklin@integrationworks.co.th>
2019-03-26 15:13:40 +0700

    initial

--- src/main/java/com/aws/codestar/projecttemplates/GatewayResponse.java
+++ src/main/java/com/aws/codestar/projecttemplates/GatewayResponse.java
@@ -3,6 +3,7 @@ package com.aws.codestar.projecttemplates;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * POJO containing response object for API Gateway.
@@ -13,10 +14,17 @@ public class GatewayResponse {
     private final Map<String, String> headers;
     private final int statusCode;
 
+    private AtomicInteger counter = new AtomicInteger();
+
     public GatewayResponse(final String body, final Map<String, String> headers, final int statusCode) {
         this.statusCode = statusCode;
         this.body = body;
+
         this.headers = Collections.unmodifiableMap(new HashMap<>(headers));
+
+        if(counter.incrementAndGet()==25000000){
+
+        }
     }
 
     public String getBody() {
